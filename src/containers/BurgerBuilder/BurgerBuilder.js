c2ff2cbb83870152e6de90d564d296d436e89ac4
louis <louisasare@hotmail.com>
2022-05-05 00:37:51 +0100

    import BurgerBuilder component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Aux from '../../hoc/Aux'
+class BurgerBuilder extends Component{
+    render (){
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>build controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
